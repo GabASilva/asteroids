659bef38dcb6027661df2c89b25b9fc6f9418775
GabASilva <gabalvessilva@gmail.com>
2026-01-22 13:43:29 -0300

    added more parameters in constants.py, added player character on main and updated player.py to be able to control de player character

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
-	a = 0
+	player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
 	pygame.init()
+	clock = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
-	while (a < 1):
+	while (True):
 		log_state()
+		screen.fill("black")
+		player.draw(screen)
 		for event in pygame.event.get():
+			player.update(dt)
 			if event.type == pygame.QUIT:
 				return
 			pass
-		screen.fill("black")
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
+		
 
 if __name__ == "__main__":
     main()
